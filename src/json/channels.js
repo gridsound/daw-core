90aedf6934a9c54710e2811834be579a33477c44
Thomas Tortorini <thomastortorini@gmail.com>
2019-03-09 07:32:45 +0100

    JSON: channels, change names

--- src/json/channels.js
+++ src/json/channels.js
@@ -3,10 +3,10 @@
 DAWCore.json.channels = () => {
 	const chans = {
 		"main": DAWCore.json.channel( 0, "main" ),
-		"1": DAWCore.json.channel( 1, "chan#1" ),
-		"2": DAWCore.json.channel( 2, "chan#2" ),
-		"3": DAWCore.json.channel( 3, "chan#3" ),
-		"4": DAWCore.json.channel( 4, "chan#4" ),
+		"1": DAWCore.json.channel( 1, "chan 1" ),
+		"2": DAWCore.json.channel( 2, "chan 2" ),
+		"3": DAWCore.json.channel( 3, "chan 3" ),
+		"4": DAWCore.json.channel( 4, "chan 4" ),
 	};
 
 	delete chans.main.dest;
