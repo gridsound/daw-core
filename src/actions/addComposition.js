8bbd78e2819a025483e00730eb7267b16e821b6a
Thomas Tortorini <thomastortorini@gmail.com>
2019-03-24 16:55:33 +0100

    .addComposition: _call("compositionSavedStatus")

--- src/actions/addComposition.js
+++ src/actions/addComposition.js
@@ -8,5 +8,6 @@ DAWCore.prototype.addComposition = function( cmp, opt = {} ) {
 		}, opt ) );
 	this.cmps[ cpy.options.saveMode ].set( cpy.id, cpy );
 	this._call( "compositionAdded", cpy );
+	this._call( "compositionSavedStatus", cpy, true );
 	return Promise.resolve( cpy );
 };
