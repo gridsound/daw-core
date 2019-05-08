876059acc49952e9ba5878e10db54de3422fe0d1
Thomas Tortorini <thomastortorini@gmail.com>
2019-05-09 01:19:21 +0200

    DAWCore.objectDeepFreeze: 'Object' -> 'object'

--- src/utils/objectDeepFreeze.js
+++ src/utils/objectDeepFreeze.js
@@ -1,7 +1,7 @@
 "use strict";
 
 DAWCore.objectDeepFreeze = obj => {
-	if ( obj && typeof obj === "Object" && !Array.isArray( obj ) ) {
+	if ( obj && typeof obj === "object" && !Array.isArray( obj ) ) {
 		Object.values( obj ).forEach( val => DAWCore.freezeObject( val ) );
 	}
 	return Object.freeze( obj );
