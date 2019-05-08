bdf703c10621b7d2971efb6503d9828e5f2a13e9
Thomas Tortorini <thomastortorini@gmail.com>
2019-05-09 01:17:48 +0200

    DAWCore.uuid: simplify the function

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,11 +1,17 @@
 "use strict";
 
-DAWCore.uuid = () => (
-	"xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
-		.replace( /[xy]/g, c => {
-			const r = Math.random() * 16 | 0,
-				v = c === "x" ? r : ( r & 0x3 | 0x8 );
+DAWCore.uuid = () => {
+	const rnd = crypto.getRandomValues( new Uint8Array( 36 ) ),
+		uuid = rnd.reduce( ( arr, n ) => {
+			arr.push( ( n % 16 ).toString( 16 ) );
+			return arr;
+		}, [] );
 
-			return v.toString( 16 );
-		} )
-);
+	uuid[ 14 ] = "4";
+	uuid[ 19 ] = ( 8 + rnd[ 19 ] % 4 ).toString( 16 );
+	uuid[ 8 ] =
+	uuid[ 13 ] =
+	uuid[ 18 ] =
+	uuid[ 23 ] = "-";
+	return uuid.join( "" );
+};
