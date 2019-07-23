4fcd985b40c88f7e9c230c68eefb3155a7300028
Thomas Tortorini <thomastortorini@gmail.com>
2019-07-24 00:08:52 +0200

    dropAudioFiles

---
+++ src/actions/dropAudioFiles.js
@@ -0,0 +1,53 @@
+"use strict";
+
+DAWCore.prototype.dropAudioFiles = function( files ) {
+	this.buffers.loadFiles( files ).then( ( { newBuffers, knownBuffers, failedBuffers } ) => {
+		if ( newBuffers.length || knownBuffers.length ) {
+			const cmpBuffers = this.get.buffers(),
+				bufNextId = +this._getNextIdOf( cmpBuffers ),
+				patNextId = +this._getNextIdOf( this.get.patterns() ),
+				buffersLoaded = {};
+
+			if ( newBuffers.length ) {
+				const obj = {};
+
+				obj.buffers = {};
+				obj.patterns = {};
+				newBuffers.forEach( ( buf, i ) => {
+					obj.buffers[ bufNextId + i ] = {
+						type: buf.type,
+						name: buf.name,
+						length: buf.length,
+						duration: buf.duration,
+					};
+					obj.patterns[ patNextId + i ] = {
+						type: "buffer",
+						name: buf.name,
+						buffer: `${ bufNextId + i }`,
+						duration: Math.ceil( buf.duration * ( this.get.bpm() / 60 ) ),
+					};
+					buffersLoaded[ bufNextId + i ] = this.buffers.getBuffer( buf );
+				} );
+				this.compositionChange( obj );
+			}
+			if ( knownBuffers.length ) {
+				const bufmap = Object.entries( cmpBuffers )
+						.reduce( ( map, [ idBuf, buf ] ) => {
+							map.set( this.buffers.getBufferKey( buf ), idBuf );
+							return map;
+						}, new Map() );
+
+				knownBuffers.forEach( buf => {
+					const idBuf = bufmap.get( this.buffers.getBufferKey( buf ) );
+
+					buffersLoaded[ idBuf ] = this.buffers.getBuffer( buf );
+				} );
+			}
+			this._call( "buffersLoaded", buffersLoaded );
+		}
+		if ( failedBuffers.length > 0 ) {
+			console.log( "failedBuffers", failedBuffers );
+			// show a popup
+		}
+	} );
+};
