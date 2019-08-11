a1b36229e1f8f6b90fa2cd67ba7d5c6c1240b094
Thomas Tortorini <thomastortorini@gmail.com>
2019-08-12 00:08:06 +0200

    changePatternSynth: simpler

--- src/actions/changePatternSynth.js
+++ src/actions/changePatternSynth.js
@@ -1,11 +1,7 @@
 "use strict";
 
 DAWCore.prototype.changePatternSynth = function( id, synth ) {
-	const pat = this.get.pattern( id );
-
-	if ( !pat ) {
-		this._error( "changePatternSynth", "patterns", id );
-	} else if ( pat.synth !== synth ) {
+	if ( this.get.pattern( id ).synth !== synth ) {
 		const obj = { patterns: { [ id ]: { synth } } };
 
 		if ( id === this.get.patternKeysOpened() ) {
