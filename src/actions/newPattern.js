539bb806580841f3ccec539a3099526932070515
Thomas Tortorini <thomastortorini@gmail.com>
2019-08-19 02:54:17 +0200

    newPattern: order + remove check

--- src/actions/newPattern.js
+++ src/actions/newPattern.js
@@ -1,26 +1,25 @@
 "use strict";
 
 DAWCore.prototype.newPattern = function( synthId ) {
-	const syn = this.get.synth( synthId );
+	const pats = this.get.patterns(),
+		keysId = this._getNextIdOf( this.get.keys() ),
+		patId = this._getNextIdOf( pats ),
+		order = Object.values( pats ).reduce( ( max, pat ) => {
+			return pat.synth !== synthId
+				? max
+				: Math.max( max, pat.order );
+		}, 0 ) + 1;
 
-	syn
-		? this.compositionChange( this._newPattern( synthId ) )
-		: this._error( "newPattern", "synths", synthId );
-};
-
-DAWCore.prototype._newPattern = function( synthId ) {
-	const keysId = this._getNextIdOf( this.get.keys() ),
-		patId = this._getNextIdOf( this.get.patterns() );
-
-	return {
+	this.compositionChange( {
 		keys: { [ keysId ]: {} },
 		patterns: { [ patId ]: {
-			name: this._createUniqueName( "patterns", "pat" ),
+			order,
 			type: "keys",
+			name: this._createUniqueName( "patterns", "pat" ),
 			keys: keysId,
 			synth: synthId,
 			duration: this.get.beatsPerMeasure(),
 		} },
 		patternKeysOpened: patId,
-	};
+	} );
 };
