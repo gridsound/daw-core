dba52893288b5e8dee328c1110d5e53d66a00a63
Thomas Tortorini <thomastortorini@gmail.com>
2019-10-01 02:46:16 +0200

    DAWCore.prototype.get: handle effects

--- src/DAWCore.prototype.get.js
+++ src/DAWCore.prototype.get.js
@@ -2,7 +2,7 @@
 
 Object.assign( DAWCore.prototype, {
 	_getInit() {
-		const listnames = [ "channel", "synth", "buffer", "pattern", "block", "track", "keys" ],
+		const listnames = "channel effect synth buffer pattern block track keys".split( " " ),
 			cmp = () => this.composition.cmp,
 			getList = list => cmp() && cmp()[ list ],
 			getObject = ( list, id ) => cmp() && cmp()[ list ][ id ],
