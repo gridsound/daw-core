0f5584e26be0a513719012e1e38137cbf83eab08
Thomas Tortorini <thomastortorini@gmail.com>
2019-11-23 22:17:50 +0100

    JSON: channels, reduce the main's gain

--- src/json/channels.js
+++ src/json/channels.js
@@ -1,15 +1,16 @@
 "use strict";
 
 DAWCore.json.channels = () => {
-	const chans = {
-		main: DAWCore.json.channel( 0, "main" ),
+	const main = DAWCore.json.channel( 0, "main" );
+
+	main.gain = .4;
+	delete main.dest;
+	delete main.order;
+	return {
+		main,
 		1: DAWCore.json.channel( 0, "chan 1" ),
 		2: DAWCore.json.channel( 1, "chan 2" ),
 		3: DAWCore.json.channel( 2, "chan 3" ),
 		4: DAWCore.json.channel( 3, "chan 4" ),
 	};
-
-	delete chans.main.order;
-	delete chans.main.dest;
-	return chans;
 };
