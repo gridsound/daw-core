bab07e7644baa66e4cfe25ad00ac8e49017407ba
Thomas Tortorini <thomastortorini@gmail.com>
2019-12-01 11:25:16 +0100

    JSON: synth, add two oscs by default

--- src/json/synth.js
+++ src/json/synth.js
@@ -4,11 +4,8 @@ DAWCore.json.synth = name => ( {
 	name,
 	dest: "main",
 	lfo: DAWCore.json.lfo(),
-	oscillators: { 0: {
-		order: 0,
-		type: "sine",
-		detune: 0,
-		pan: 0,
-		gain: 1,
-	} },
+	oscillators: {
+		0: { order: 0, type: "sine", detune: 0, pan: 0, gain: .75 },
+		1: { order: 1, type: "sine", detune: -24, pan: 0, gain: .2 },
+	},
 } );
