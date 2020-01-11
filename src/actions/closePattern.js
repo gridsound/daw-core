3775244f322748be67ca741d485adbb9d3d47123
Thomas Tortorini <thomastortorini@gmail.com>
2020-01-11 05:50:44 +0100

    closePattern

---
+++ src/actions/closePattern.js
@@ -0,0 +1,15 @@
+"use strict";
+
+DAWCore.prototype.closePattern = function( type ) {
+	const attr = type === "keys"
+			? "patternKeysOpened"
+			: type === "drums"
+				? "patternDrumsOpened"
+				: "patternBufferOpened"
+
+	if ( this.get[ attr ]() ) {
+		const obj = { [ attr ]: null };
+
+		this.composition.change( obj, DAWCore.composeUndo( this.get.composition(), obj ) );
+	}
+};
