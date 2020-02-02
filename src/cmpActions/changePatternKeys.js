b94278a14604206546bd28d13bfd27d7bd857f66
Thomas Tortorini <thomastortorini@gmail.com>
2020-02-02 03:42:38 +0100

    cmpActions/changePatternKeys

---
+++ src/cmpActions/changePatternKeys.js
@@ -0,0 +1,28 @@
+"use strict";
+
+DAWCore.actions.changePatternKeys = function( patId, keysObj, duration ) {
+	const pat = this.get.pattern( patId ),
+		keys = this.get.keys( pat.keys ),
+		obj = { keys: { [ pat.keys ]: keysObj } };
+
+	if ( duration !== pat.duration ) {
+		const objPatterns = { [ patId ]: { duration } },
+			cmpDur = this.composition.getNewDuration( objPatterns ),
+			objBlocks = Object.entries( this.get.blocks() )
+				.reduce( ( obj, [ id, blc ] ) => {
+					if ( blc.pattern === patId && !blc.durationEdited ) {
+						obj[ id ] = { duration };
+					}
+					return obj;
+				}, {} );
+
+		obj.patterns = objPatterns;
+		DAWCore.utils.addIfNotEmpty( obj, "blocks", objBlocks );
+		if ( Math.abs( cmpDur - this.get.duration() ) > .001 ) {
+			obj.duration = cmpDur;
+		}
+	}
+	return [
+		obj,
+	];
+};
