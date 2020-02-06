e70ba21341d434adf75464bb43f4748bbdb7cfb0
Thomas Tortorini <thomastortorini@gmail.com>
2020-02-06 18:56:24 +0100

    cmpActions/addDrumrow: fix

--- src/cmpActions/addDrumrow.js
+++ src/cmpActions/addDrumrow.js
@@ -1,13 +1,13 @@
 "use strict";
 
-DAWCore.actions.addDrumrow = function( patternId ) {
+DAWCore.actions.addDrumrow = function( pattern ) {
 	const drumrows = this.get.drumrows(),
-		patName = this.get.drumrow( patternId ).name,
+		patName = this.get.pattern( pattern ).name,
 		rowId = this._getNextIdOf( drumrows ),
 		rowObj = {
 			order: this._getNextOrderOf( drumrows ),
 			toggle: true,
-			pattern: patternId,
+			pattern,
 			gain: 1,
 			pan: 0,
 		};
