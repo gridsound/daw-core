ae5dbf3813ec0a8672b5cef7f386e8ef39de7112
Thomas Tortorini <thomastortorini@gmail.com>
2020-02-07 07:38:35 +0100

    cmpActions/common/calcNewDuration: arrow func

--- src/cmpActions/common/calcNewDuration.js
+++ src/cmpActions/common/calcNewDuration.js
@@ -1,14 +1,13 @@
 "use strict";
 
-DAWCore.common.calcNewDuration = function( get, newPatDurations ) {
+DAWCore.common.calcNewDuration = ( get, newPatDurations ) => {
 	const bPM = get.beatsPerMeasure(),
-		dur = Object.values( get.blocks() )
-			.reduce( ( max, blc ) => {
-				const pat = newPatDurations[ blc.pattern ],
-					dur = ( pat && !blc.durationEdited ? pat : blc ).duration;
+		dur = Object.values( get.blocks() ).reduce( ( max, blc ) => {
+			const pat = newPatDurations[ blc.pattern ],
+				dur = ( pat && !blc.durationEdited ? pat : blc ).duration;
 
-				return Math.max( max, blc.when + dur );
-			}, 0 );
+			return Math.max( max, blc.when + dur );
+		}, 0 );
 
 	return Math.ceil( dur / bPM ) * bPM;
 };
