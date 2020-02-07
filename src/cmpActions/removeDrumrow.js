7540275606419cee7272337f3cd49ccb39735c59
Thomas Tortorini <thomastortorini@gmail.com>
2020-02-07 13:34:24 +0100

    cmpActions/removeDrumrow: handle durations change

--- src/cmpActions/removeDrumrow.js
+++ src/cmpActions/removeDrumrow.js
@@ -1,23 +1,59 @@
 "use strict";
 
-DAWCore.actions.removeDrumrow = function( rowId ) {
-	const patName = this._getPatByRowId( rowId ).name,
-		drums = Object.entries( this.get.drums() ).reduce( ( obj, [ drumsId, drums ] ) => {
-			const drumsObj = Object.entries( drums ).reduce( ( obj, [ drumId, drum ] ) => {
-				if ( drum.row === rowId ) {
-					obj[ drumId ] = undefined;
-				}
-				return obj;
-			}, {} );
+DAWCore.actions.removeDrumrow = ( rowId, get ) => {
+	const patName = DAWCore.common.getDrumrowName( rowId, get );
 
-			DAWCore.utils.addIfNotEmpty( obj, drumsId, drumsObj );
-			return obj;
-		}, {} ),
-		obj = { drumrows: { [ rowId ]: undefined } };
-
-	DAWCore.utils.addIfNotEmpty( obj, "drums", drums );
 	return [
-		obj,
+		DAWCore.actions._removeDrumrow( rowId, get ),
 		[ "drumrows", "removeDrumrow", patName ],
 	];
+};
+
+DAWCore.actions._removeDrumrow = ( rowId, get ) => {
+	const bPM = get.beatsPerMeasure(),
+		blocksEnt = Object.entries( get.blocks() ),
+		patternsEnt = Object.entries( get.patterns() ),
+		obj = { drumrows: { [ rowId ]: undefined } },
+		objDrums = {},
+		objBlocks = {},
+		objPatterns = {};
+
+	patternsEnt.forEach( ( [ patId, pat ] ) => {
+		if ( pat.type === "drums" ) {
+			const drumsObj = {},
+				drumWhenMax = Object.entries( get.drums( pat.drums ) )
+					.reduce( ( max, [ drumId, drum ] ) => {
+						if ( drum.row === rowId ) {
+							drumsObj[ drumId ] = undefined;
+							return max;
+						}
+						return Math.max( max, drum.when + .001 );
+					}, 0 );
+
+			if ( DAWCore.utils.isntEmpty( drumsObj ) ) {
+				const duration = Math.max( 1, Math.ceil( drumWhenMax / bPM ) ) * bPM;
+
+				objDrums[ pat.drums ] = drumsObj;
+				if ( duration !== pat.duration ) {
+					objPatterns[ patId ] = { duration };
+					blocksEnt.forEach( ( [ blcId, blc ] ) => {
+						if ( blc.pattern === patId && !blc.durationEdited ) {
+							objBlocks[ blcId ] = { duration };
+						}
+					} );
+				}
+			}
+		}
+	} );
+	DAWCore.utils.addIfNotEmpty( obj, "drums", objDrums );
+	DAWCore.utils.addIfNotEmpty( obj, "patterns", objPatterns );
+	if ( DAWCore.utils.isntEmpty( objBlocks ) ) {
+		const duration = DAWCore.common.calcNewDuration( objPatterns, get );
+
+		obj.blocks = objBlocks;
+		if ( duration !== get.duration() ) {
+			obj.duration = duration;
+		}
+	}
+	return obj;
 };
