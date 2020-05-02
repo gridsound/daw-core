7ec6a1d4d1a4ef043889257cc9f50dc1b9fd8eda
Thomas Tortorini <thomastortorini@gmail.com>
2020-05-02 04:50:13 +0200

    cmpActions/changeTempo: if the beatsPerMeasure changes then all the patterns have to recalc their own duration

--- src/cmpActions/changeTempo.js
+++ src/cmpActions/changeTempo.js
@@ -2,49 +2,58 @@
 
 DAWCore.actions.changeTempo = ( bpm, bPM, sPB, get ) => {
 	const bpmChanged = bpm !== get.bpm(),
-		timeSignChanged =
+		signChanged =
 			bPM !== get.beatsPerMeasure() ||
 			sPB !== get.stepsPerBeat();
 
-	if ( timeSignChanged || bpmChanged ) {
-		const obj = {};
+	if ( signChanged || bpmChanged ) {
+		const obj = {},
+			objPatterns = {},
+			patts = Object.entries( get.patterns() );
 
-		if ( timeSignChanged ) {
+		if ( signChanged ) {
 			obj.beatsPerMeasure = bPM;
 			obj.stepsPerBeat = sPB;
-		}
-		if ( bpmChanged ) {
-			const objPatterns = {};
-
-			obj.bpm = bpm;
-			Object.entries( get.patterns() ).forEach( ( [ id, pat ] ) => {
-				if ( pat.type === "buffer" ) {
-					const buf = get.buffer( pat.buffer ),
-						duration = Math.ceil( buf.duration * ( bpm / 60 ) );
+			patts.forEach( ( [ id, pat ] ) => {
+				if ( pat.type === "keys" || pat.type === "drums" ) {
+					const duration = Math.max( 1, Math.ceil( pat.duration / bPM ) ) * bPM;
 
 					if ( duration !== pat.duration ) {
 						objPatterns[ id ] = { duration };
 					}
 				}
 			} );
-			if ( GSUtils.isntEmpty( objPatterns ) ) {
-				const objBlocks = {};
+		}
+		if ( bpmChanged ) {
+			obj.bpm = bpm;
+			patts.forEach( ( [ id, pat ] ) => {
+				if ( pat.type === "buffer" ) {
+					const bufDur = get.buffer( pat.buffer ).duration,
+						duration = Math.ceil( bufDur * ( bpm / 60 ) );
 
-				obj.patterns = objPatterns;
-				Object.entries( get.blocks() ).forEach( ( [ id, blc ] ) => {
-					const pat = objPatterns[ blc.pattern ];
-
-					if ( pat && !blc.durationEdited ) {
-						objBlocks[ id ] = { duration: pat.duration };
+					if ( duration !== pat.duration ) {
+						objPatterns[ id ] = { duration };
 					}
-				} );
-				if ( GSUtils.isntEmpty( objBlocks ) ) {
-					const cmpDur = DAWCore.common.calcNewDuration( objPatterns, get );
+				}
+			} );
+		}
+		if ( GSUtils.isntEmpty( objPatterns ) ) {
+			const objBlocks = {};
 
-					obj.blocks = objBlocks;
-					if ( cmpDur !== get.duration() ) {
-						obj.duration = cmpDur;
-					}
+			obj.patterns = objPatterns;
+			Object.entries( get.blocks() ).forEach( ( [ id, blc ] ) => {
+				const pat = objPatterns[ blc.pattern ];
+
+				if ( pat && !blc.durationEdited ) {
+					objBlocks[ id ] = { duration: pat.duration };
+				}
+			} );
+			if ( GSUtils.isntEmpty( objBlocks ) ) {
+				const cmpDur = DAWCore.common.calcNewDuration( objPatterns, get );
+
+				obj.blocks = objBlocks;
+				if ( cmpDur !== get.duration() ) {
+					obj.duration = cmpDur;
 				}
 			}
 		}
