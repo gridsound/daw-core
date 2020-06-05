77e55a38252697eeb9bb87341a0ff51bf4e1a744
Thomas Tortorini <thomastortorini@gmail.com>
2020-06-05 06:26:03 +0200

    cmpActions/redirectToChannel: prevent redirecting a chan into itself

--- src/cmpActions/redirectToChannel.js
+++ src/cmpActions/redirectToChannel.js
@@ -1,12 +1,14 @@
 "use strict";
 
 DAWCore.actions.redirectToChannel = ( family, id, dest, get ) => {
-	const node = get[ family ]( id ),
-		chanName = get.channel( dest ).name,
-		families = `${ family }s`;
+	if ( family !== "channel" || id !== dest ) {
+		const node = get[ family ]( id ),
+			chanName = get.channel( dest ).name,
+			families = `${ family }s`;
 
-	return [
-		{ [ families ]: { [ id ]: { dest } } },
-		[ families, `redirect${ GSUtils.capitalize( family ) }`, node.name, chanName ],
-	];
+		return [
+			{ [ families ]: { [ id ]: { dest } } },
+			[ families, `redirect${ GSUtils.capitalize( family ) }`, node.name, chanName ],
+		];
+	}
 };
