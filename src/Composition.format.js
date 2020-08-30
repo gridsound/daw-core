3735258fea9b42c535d41189bc94ab6c701f0883
Thomas Tortorini <thomastortorini@gmail.com>
2020-08-30 22:45:13 +0200

    Composition.format: add env if not present

--- src/Composition.format.js
+++ src/Composition.format.js
@@ -83,6 +83,7 @@ DAWCore.Composition.format = function( cmp ) {
 	}
 	Object.values( cmp.synths ).forEach( syn => {
 		delete syn.envelopes;
+		syn.env = syn.env || DAWCore.json.env();
 		syn.lfo = syn.lfo || DAWCore.json.lfo();
 		Object.values( syn.oscillators ).forEach( osc => {
 			osc.detune = Math.min( Math.max( -24, Math.round( osc.detune ) ), 24 );
