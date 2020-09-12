96be684fed7dc18269da20281a226d7663f9d66a
Thomas Tortorini <thomastortorini@gmail.com>
2020-09-12 17:50:37 +0200

    actions/common/toggleSolo

--- src/actions/toggleOnlyDrumrow.js
+++ src/actions/toggleOnlyDrumrow.js
@@ -2,19 +2,10 @@
 
 DAWCore.actions.toggleOnlyDrumrow = ( rowId, get ) => {
 	const patName = DAWCore.common.getDrumrowName( rowId, get ),
-		entries = Object.entries( get.drumrows() ),
-		someOn = entries.some( kv => kv[ 0 ] !== rowId && kv[ 1 ].toggle === true ),
-		drumrows = entries.reduce( ( obj, [ id, row ] ) => {
-			const itself = id === rowId;
-
-			if ( ( itself && !row.toggle ) || ( !itself && row.toggle === someOn ) ) {
-				obj[ id ] = { toggle: !row.toggle };
-			}
-			return obj;
-		}, {} );
+		[ someOn, drumrows ] = DAWCore.common.toggleSolo( rowId, get.drumrows() );
 
 	return [
 		{ drumrows },
-		[ "drumrows", "toggleOnlyDrumrow", patName, !someOn ],
+		[ "drumrows", "toggleOnlyDrumrow", patName, someOn ],
 	];
 };
