e721af21f7a9cd227851e99c7aeb82b4493d5f10
Thomas Tortorini <thomastortorini@gmail.com>
2020-09-12 17:13:23 +0200

    json/block: null instead of empty string for ids

--- src/json/block.js
+++ src/json/block.js
@@ -1,12 +1,12 @@
 "use strict";
 
 DAWCore.json.block = obj => ( {
-	pattern: "",
+	pattern: null,
 	duration: 0,
 	durationEdited: false,
 	selected: false,
 	offset: 0,
 	when: 0,
-	track: "",
+	track: null,
 	...obj,
 } );
