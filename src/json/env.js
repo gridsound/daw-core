7d6036e9ab6722b5076bf0c33caf895259c2a25b
mr21 <thomastortorini@gmail.com>
2021-04-25 07:18:47 +0200

    json/env: change the default values

--- src/json/env.js
+++ src/json/env.js
@@ -2,10 +2,10 @@
 
 DAWCore.json.env = obj => ( {
 	toggle: true,
-	attack: .15,
-	hold: .08,
-	decay: .08,
-	substain: .5,
+	attack: .1,
+	hold: .05,
+	decay: .1,
+	substain: .6,
 	release: .25,
 	...obj,
 } );
