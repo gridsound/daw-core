afcc6d77307a6a19c3fd9f60d5c6e40ac70df7df
mr21 <thomastortorini@gmail.com>
2021-04-26 04:02:24 +0200

    prototype/exportCompositionToWAV: no need to care about the gain here

--- src/prototype/exportCompositionToWAV.js
+++ src/prototype/exportCompositionToWAV.js
@@ -2,7 +2,6 @@
 
 DAWCore.prototype.exportCompositionToWAV = function() {
 	const ctx = this.ctx,
-		gain = this.destination.getGain(),
 		dur = Math.ceil( this.get.duration() * 60 / this.get.bpm() ) || 1,
 		ctxOff = new OfflineAudioContext( 2, dur * ctx.sampleRate | 0, ctx.sampleRate );
 
@@ -11,7 +10,6 @@ DAWCore.prototype.exportCompositionToWAV = function() {
 		URL.revokeObjectURL( DAWCore._URLToRevoke );
 	}
 	this.setCtx( ctxOff );
-	this.destination.setGain( 1 );
 	this.composition.play();
 	return ctxOff.startRendering().then( buffer => {
 		const pcm = gswaEncodeWAV.encode( buffer, { float32: true } ),
@@ -19,7 +17,6 @@ DAWCore.prototype.exportCompositionToWAV = function() {
 
 		this.composition.stop();
 		this.setCtx( ctx );
-		this.destination.setGain( gain );
 		DAWCore._URLToRevoke = url;
 		return {
 			url,
