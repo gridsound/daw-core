629dbeb72db4fbd964386d49b412bfc96d89e6e5
mr21 <thomastortorini@gmail.com>
2021-10-07 05:36:25 +0200

    actions/openPattern: simplify

--- src/actions/openPattern.js
+++ src/actions/openPattern.js
@@ -1,28 +1,15 @@
 "use strict";
 
-DAWCore.actions.openPattern = ( patId, get ) => {
-	const pat = get.pattern( patId );
+DAWCore.actions.openPattern = ( id, get ) => {
+	const pat = get.pattern( id ),
+		attr = DAWCore.actions.common.patternOpenedByType[ pat.type ],
+		obj = {};
 
-	switch ( pat.type ) {
-		case "slices":
-			if ( patId !== get.patternSlicesOpened() ) {
-				return { patternSlicesOpened: patId };
-			}
-			break;
-		case "drums":
-			if ( patId !== get.patternDrumsOpened() ) {
-				return { patternDrumsOpened: patId };
-			}
-			break;
-		case "keys":
-			if ( patId !== get.patternKeysOpened() ) {
-				const obj = { patternKeysOpened: patId };
-
-				if ( pat.synth !== get.synthOpened() ) {
-					obj.synthOpened = pat.synth;
-				}
-				return obj;
-			}
-			break;
+	if ( id !== get[ attr ]() ) {
+		obj[ attr ] = id;
+		if ( pat.type === "keys" && pat.synth !== get.synthOpened() ) {
+			obj.synthOpened = pat.synth;
+		}
 	}
+	return obj;
 };
