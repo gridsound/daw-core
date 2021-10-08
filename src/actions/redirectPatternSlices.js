d06870613d7bc41074d720dc48ee7ceedaa1ae74
mr21 <thomastortorini@gmail.com>
2021-10-08 04:48:20 +0200

    actions/redirectPatternSlices: check if the src change

--- src/actions/redirectPatternSlices.js
+++ src/actions/redirectPatternSlices.js
@@ -1,10 +1,10 @@
 "use strict";
 
 DAWCore.actions.redirectPatternSlices = ( patId, source, get ) => {
-	const obj = { patterns: { [ patId ]: { source } } };
-
-	return [
-		obj,
-		[ "patterns", "redirectPatternSlices", get.pattern( patId ).name, get.pattern( source ).name ],
-	];
+	if ( source !== get.pattern( patId ).source ) {
+		return [
+			{ patterns: { [ patId ]: { source } } },
+			[ "patterns", "redirectPatternSlices", get.pattern( patId ).name, get.pattern( source ).name ],
+		];
+	}
 };
