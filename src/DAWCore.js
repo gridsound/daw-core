ea10501b2043210c74dbe57e3bdcf8ab3be32436
mr21 <thomastortorini@gmail.com>
2022-05-01 01:59:29 +0200

    DAWCore.prototype: bring back all these files into DAWCore
    To privatise everything we can later

--- src/DAWCore.js
+++ src/DAWCore.js
@@ -143,7 +143,251 @@ class DAWCore {
 		if ( this.ctx instanceof OfflineAudioContext ) {
 			this.composition.stop();
 		}
-	};
+	}
+
+	// ..........................................................................
+	newComposition( opt ) {
+		const cmp = DAWCore.json.composition( this.env, DAWCore.utils.uuid() );
+
+		return this.addComposition( cmp, opt )
+			.then( cmp => this.composition.load( cmp ) )
+			.then( cmp => this._compositionOpened( cmp ) );
+	}
+	addComposition( cmp, opt ) {
+		const cpy = DAWCore.utils.jsonCopy( cmp );
+
+		cpy.options = Object.freeze( {
+			saveMode: "local",
+			...opt,
+		} );
+		this.cmps[ cpy.options.saveMode ].set( cpy.id, cpy );
+		this._call( "compositionAdded", cpy );
+		this._call( "compositionSavedStatus", cpy, true );
+		return Promise.resolve( cpy );
+	}
+	addCompositionByBlob( blob, opt ) {
+		return new Promise( ( res, rej ) => {
+			const rd = new FileReader();
+
+			rd.onload = () => {
+				this.addCompositionByJSON( rd.result, opt ).then( res, rej );
+			};
+			rd.readAsText( blob );
+		} );
+	}
+	addCompositionByJSON( json, opt ) {
+		return new Promise( ( res, rej ) => {
+			try {
+				const cmp = JSON.parse( json );
+
+				this.addComposition( cmp, opt ).then( res, rej );
+			} catch ( e ) {
+				rej( e );
+			}
+		} );
+	}
+	addCompositionByURL( url, opt ) {
+		return fetch( url )
+			.then( res => {
+				if ( !res.ok ) {
+					throw `The file is not accessible: ${ url }`;
+				}
+				return res.json();
+			} )
+			.then(
+				cmp => this.addComposition( cmp, opt ),
+				e => { throw e; }
+			);
+	}
+	addCompositionsFromLocalStorage() {
+		return Promise.all( DAWCore.LocalStorage
+			.getAll().map( cmp => this.addComposition( cmp ) ) );
+	}
+	addNewComposition( opt ) {
+		return this.addComposition(
+			DAWCore.json.composition( this.env, DAWCore.utils.uuid() ), opt );
+	}
+	openComposition( saveMode, id ) {
+		const cmp = this.get.composition( saveMode, id );
+
+		if ( cmp ) {
+			if ( this.composition.loaded ) {
+				this.closeComposition();
+			}
+			return ( this.get.composition( saveMode, id ) // 1.
+				? Promise.resolve( cmp )
+				: this.addNewComposition( { saveMode } ) )
+				.then( cmp => this.composition.load( cmp ) )
+				.then( cmp => this._compositionOpened( cmp ) );
+		}
+	}
+	_compositionOpened( cmp ) {
+		this.focusOn( "composition" );
+		this._call( "compositionOpened", cmp );
+		this._startLoop();
+		return cmp;
+	}
+	closeComposition() {
+		if ( this.composition.loaded ) {
+			const cmp = this.cmps[ this.get.saveMode() ].get( this.get.id() );
+
+			this.stop();
+			this.keys.clearLoop();
+			this.keys.setCurrentTime( 0 );
+			this.composition.setCurrentTime( 0 );
+			this._stopLoop();
+			this._call( "compositionClosed", cmp );
+			this.composition.unload();
+			this.history.empty();
+			this.buffers.empty();
+			if ( !cmp.savedAt ) {
+				this._deleteComposition( cmp );
+			}
+		}
+	}
+	deleteComposition( saveMode, id ) {
+		if ( this.composition.cmp && id === this.get.id() ) {
+			this.closeComposition();
+		}
+		this._deleteComposition( this.cmps[ saveMode ].get( id ) );
+	}
+	_deleteComposition( cmp ) {
+		if ( cmp ) {
+			const saveMode = cmp.options.saveMode;
+
+			this.cmps[ saveMode ].delete( cmp.id );
+			if ( saveMode === "local" ) {
+				DAWCore.LocalStorage.delete( cmp.id );
+			}
+			this._call( "compositionDeleted", cmp );
+		}
+	}
+	saveComposition() {
+		const actSave = this.composition._actionSavedOn;
+
+		if ( this.composition.save() ) {
+			const cmp = this.get.cmp();
+			const id = this.get.id();
+
+			if ( this.get.saveMode() === "local" ) {
+				this.cmps.local.set( id, cmp );
+				DAWCore.LocalStorage.put( id, cmp );
+				this._call( "compositionSavedStatus", cmp, true );
+			} else {
+				this.composition._saved = false;
+				this._call( "compositionLoading", cmp, true );
+				( this._call( "compositionSavingPromise", cmp )
+				|| Promise.resolve( cmp ) )
+					.finally( this._call.bind( this, "compositionLoading", cmp, false ) )
+					.then( res => {
+						this.composition._saved = true;
+						this.cmps.cloud.set( id, cmp );
+						this._call( "compositionSavedStatus", cmp, true );
+						return res;
+					}, err => {
+						this.composition._actionSavedOn = actSave;
+						this._call( "compositionSavedStatus", cmp, false );
+						throw err;
+					} );
+			}
+		}
+	}
+	exportCompositionToWAV() {
+		const ctx = this.ctx;
+		const dur = Math.ceil( this.get.duration() / this.get.bps() ) || 1;
+		const ctxOff = new OfflineAudioContext( 2, dur * ctx.sampleRate | 0, ctx.sampleRate );
+
+		this.stop();
+		if ( DAWCore._URLToRevoke ) {
+			URL.revokeObjectURL( DAWCore._URLToRevoke );
+		}
+		this.setCtx( ctxOff );
+		this.composition.play();
+		return ctxOff.startRendering().then( buffer => {
+			const pcm = gswaEncodeWAV.encode( buffer, { float32: true } );
+			const url = URL.createObjectURL( new Blob( [ pcm ] ) );
+
+			this.composition.stop();
+			this.setCtx( ctx );
+			DAWCore._URLToRevoke = url;
+			return {
+				url,
+				name: `${ this.get.name() || "untitled" }.wav`,
+			};
+		} );
+	}
+
+	// ..........................................................................
+	liveChangeChannel( id, prop, val ) {
+		this.composition._wamixer.change( { channels: { [ id ]: { [ prop ]: val } } } );
+	}
+	liveChangeEffect( fxId, prop, val ) {
+		this.composition._waeffects.liveChangeFxProp( fxId, prop, val );
+	}
+	liveChangeSynth( id, obj ) {
+		this.composition._synths.get( id ).change( obj );
+	}
+
+	// ..........................................................................
+	dropAudioFiles( files ) {
+		const order = this.buffers.getSize();
+
+		this.buffers.loadFiles( files ).then( ( { newBuffers, knownBuffers, failedBuffers } ) => {
+			if ( newBuffers.length || knownBuffers.length ) {
+				const cmpBuffers = this.get.buffers();
+				const bufNextId = +DAWCore.actionsCommon.getNextIdOf( cmpBuffers );
+				const patNextId = +DAWCore.actionsCommon.getNextIdOf( this.get.patterns() );
+				const buffersLoaded = {};
+
+				if ( newBuffers.length ) {
+					const obj = {};
+
+					obj.buffers = {};
+					obj.patterns = {};
+					newBuffers.forEach( ( buf, i ) => {
+						const dotind = buf.name.lastIndexOf( "." );
+						const patname = dotind > -1 ? buf.name.substr( 0, dotind ) : buf.name;
+						const bufId = bufNextId + i;
+
+						obj.buffers[ bufId ] = {
+							MIME: buf.MIME,
+							duration: buf.duration,
+							hash: buf.hash,
+						};
+						obj.patterns[ patNextId + i ] = {
+							type: "buffer",
+							dest: "main",
+							buffer: `${ bufId }`,
+							duration: Math.ceil( buf.duration * this.get.bps() ),
+							name: patname,
+							order: order + i,
+						};
+						buffersLoaded[ bufId ] = this.buffers.getBuffer( buf );
+					} );
+					this.callAction( "dropBuffers", obj );
+				}
+				if ( knownBuffers.length ) {
+					const bufmap = Object.entries( cmpBuffers )
+						.reduce( ( map, [ idBuf, buf ] ) => {
+							map.set( buf.hash, idBuf );
+							return map;
+						}, new Map() );
+
+					knownBuffers.forEach( buf => {
+						const idBuf = bufmap.get( buf.hash );
+
+						buffersLoaded[ idBuf ] = this.buffers.getBuffer( buf );
+					} );
+					this.buffersSlices.buffersLoaded( buffersLoaded );
+				}
+				this._call( "buffersLoaded", buffersLoaded );
+			}
+			if ( failedBuffers.length > 0 ) {
+				console.log( "failedBuffers", failedBuffers );
+				// show a popup
+			}
+		} );
+	}
 
 	// ..........................................................................
 	getFocusedObject() {
