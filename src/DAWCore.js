dfec55d51c36d35b927147bfec9ff7041ebdd338
mr21 <thomastortorini@gmail.com>
2022-05-07 07:34:18 +0200

    DAWCore.exportCompositionToWAV -> DAWCore.compositionExportWAV

--- src/DAWCore.js
+++ src/DAWCore.js
@@ -160,6 +160,9 @@ class DAWCore {
 	compositionExportJSON( saveMode, id ) {
 		return DAWCoreCompositionExportJSON.export( this.get.composition( saveMode, id ) );
 	}
+	compositionExportWAV() {
+		return DAWCoreCompositionExportWAV.export( this );
+	}
 
 	// ..........................................................................
 	setCtx( ctx ) {
@@ -355,30 +358,6 @@ class DAWCore {
 					} );
 			}
 		}
-	}
-	exportCompositionToWAV() {
-		const ctx = this.ctx;
-		const dur = Math.ceil( this.get.duration() / this.get.bps() ) || 1;
-		const ctxOff = new OfflineAudioContext( 2, dur * ctx.sampleRate | 0, ctx.sampleRate );
-
-		this.stop();
-		if ( DAWCore._URLToRevoke ) {
-			URL.revokeObjectURL( DAWCore._URLToRevoke );
-		}
-		this.setCtx( ctxOff );
-		this.composition.play();
-		return ctxOff.startRendering().then( buffer => {
-			const pcm = gswaEncodeWAV.encode( buffer, { float32: true } );
-			const url = URL.createObjectURL( new Blob( [ pcm ] ) );
-
-			this.composition.stop();
-			this.setCtx( ctx );
-			DAWCore._URLToRevoke = url;
-			return {
-				url,
-				name: `${ this.get.name() || "untitled" }.wav`,
-			};
-		} );
 	}
 
 	// ..........................................................................
---
+++ src/DAWCoreCompositionExportWAV.js
@@ -0,0 +1,30 @@
+"use strict";
+
+class DAWCoreCompositionExportWAV {
+	static #URLToRevoke = null;
+
+	static export( daw ) {
+		const ctx = daw.ctx;
+		const dur = Math.ceil( daw.get.duration() / daw.get.bps() ) || 1;
+		const ctxOff = new OfflineAudioContext( 2, dur * ctx.sampleRate | 0, ctx.sampleRate );
+
+		daw.stop();
+		if ( DAWCoreCompositionExportWAV.#URLToRevoke ) {
+			URL.revokeObjectURL( DAWCoreCompositionExportWAV.#URLToRevoke );
+		}
+		daw.setCtx( ctxOff );
+		daw.composition.play();
+		return ctxOff.startRendering().then( buffer => {
+			const pcm = gswaEncodeWAV.encode( buffer, { float32: true } );
+			const url = URL.createObjectURL( new Blob( [ pcm ] ) );
+
+			daw.composition.stop();
+			daw.setCtx( ctx );
+			DAWCoreCompositionExportWAV.#URLToRevoke = url;
+			return {
+				url,
+				name: `${ daw.get.name() || "untitled" }.wav`,
+			};
+		} );
+	}
+}
