c28ba53d426124319db16f021510ee60b2bdf7f6
mr21 <thomastortorini@gmail.com>
2023-10-16 02:06:37 +0200

    actions/addOscillatorSource

--- src/actions/addOscillatorSource.js
+++ src/actions/addOscillatorSource.js
@@ -1,14 +1,16 @@
 "use strict";
 
-DAWCoreActions.set( "addOscillator", ( daw, synthId ) => {
+DAWCoreActions.set( "addOscillatorSource", ( daw, synthId, sourcePatId ) => {
 	const syn = daw.$getSynth( synthId );
 	const id = DAWCoreActionsCommon.getNextIdOf( syn.oscillators );
 	const osc = DAWCoreJSON.oscillator( {
+		wave: null,
+		source: sourcePatId,
 		order: DAWCoreActionsCommon.getNextOrderOf( syn.oscillators ),
 	} );
 
 	return [
 		{ synths: { [ synthId ]: { oscillators: { [ id ]: osc } } } },
-		[ "synth", "addOscillator", syn.name ],
+		[ "synth", "addOscillatorSource", syn.name, daw.$getPattern( sourcePatId ).name ],
 	];
 } );
