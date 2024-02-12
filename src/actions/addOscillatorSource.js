3d001fa4a4121c41f5993c8787c5da031517360b
mr21 <thomastortorini@gmail.com>
2024-02-12 08:04:09 +0100

    actions/addOscillatorSource: use of common/addPatternBuffer

--- src/actions/addOscillatorSource.js
+++ src/actions/addOscillatorSource.js
@@ -1,16 +1,20 @@
 "use strict";
 
-DAWCoreActions.set( "addOscillatorSource", ( daw, synthId, sourcePatId ) => {
+DAWCoreActions.set( "addOscillatorSource", ( daw, synthId, srcType, srcPatId ) => {
 	const syn = daw.$getSynth( synthId );
-	const id = DAWCoreActionsCommon.getNextIdOf( syn.oscillators );
-	const osc = DAWCoreJSON.oscillator( {
-		wave: null,
-		source: sourcePatId,
-		order: DAWCoreActionsCommon.getNextOrderOf( syn.oscillators ),
-	} );
+	const newOscId = DAWCoreActionsCommon.getNextIdOf( syn.oscillators );
 
-	return [
-		{ synths: { [ synthId ]: { oscillators: { [ id ]: osc } } } },
-		[ "synth", "addOscillatorSource", syn.name, daw.$getPattern( sourcePatId ).name ],
-	];
+	return DAWCoreActionsCommon.addPatternBuffer( daw, srcType, srcPatId )
+		.then( ( [ newSrcPatId, newSrcPatName, newPatObj ] ) => {
+			const newOsc = DAWCoreJSON.oscillator( {
+				wave: null,
+				source: newSrcPatId,
+				order: DAWCoreActionsCommon.getNextOrderOf( syn.oscillators ),
+			} );
+
+			return [
+				Object.assign( { synths: { [ synthId ]: { oscillators: { [ newOscId ]: newOsc } } } }, newPatObj ),
+				[ "synth", "addOscillatorSource", syn.name, newSrcPatName ],
+			];
+		} );
 } );
