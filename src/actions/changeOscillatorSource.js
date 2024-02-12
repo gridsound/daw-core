11464a94a0a4eeba476614393c2df3bdd4373345
mr21 <thomastortorini@gmail.com>
2024-02-12 08:03:56 +0100

    actions/changeOscillatorSource: use of common/addPatternBuffer

--- src/actions/changeOscillatorSource.js
+++ src/actions/changeOscillatorSource.js
@@ -1,27 +1,30 @@
 "use strict";
 
-DAWCoreActions.set( "changeOscillatorSource", ( daw, synthId, oscId, patId, resetOsc ) => {
-	const syn = daw.$getSynth( synthId );
-	const oscOri = syn.oscillators[ oscId ];
-	const osc = {};
+DAWCoreActions.set( "changeOscillatorSource", ( daw, synthId, oscId, srcType, srcPatId, resetOsc ) => {
+	return DAWCoreActionsCommon.addPatternBuffer( daw, srcType, srcPatId )
+		.then( ( [ newSrcPatId, newSrcPatName, newPatObj ] ) => {
+			const syn = daw.$getSynth( synthId );
+			const oscOri = syn.oscillators[ oscId ];
+			const osc = {};
 
-	if ( oscOri.source !== patId ) {
-		osc.source = patId;
-	}
-	if ( oscOri.wave ) {
-		osc.wave = null;
-	}
-	if ( resetOsc ) {
-		if ( oscOri.pan !== 0 ) { osc.pan = 0; }
-		if ( oscOri.gain !== 1 ) { osc.gain = 1; }
-		if ( oscOri.detune !== 0 ) { osc.detune = 0; }
-		if ( oscOri.detunefine !== 0 ) { osc.detunefine = 0; }
-		if ( oscOri.unisonvoices !== 1 ) { osc.unisonvoices = 1; }
-	}
-	if ( GSUisntEmpty( osc ) ) {
-		return [
-			{ synths: { [ synthId ]: { oscillators: { [ oscId ]: osc } } } },
-			[ "synth", "changeOscillatorSource", syn.name, daw.$getPattern( patId ).name ],
-		];
-	}
+			if ( oscOri.source !== newSrcPatId ) {
+				osc.source = newSrcPatId;
+			}
+			if ( oscOri.wave ) {
+				osc.wave = null;
+			}
+			if ( resetOsc ) {
+				if ( oscOri.pan !== 0 ) { osc.pan = 0; }
+				if ( oscOri.gain !== 1 ) { osc.gain = 1; }
+				if ( oscOri.detune !== 0 ) { osc.detune = 0; }
+				if ( oscOri.detunefine !== 0 ) { osc.detunefine = 0; }
+				if ( oscOri.unisonvoices !== 1 ) { osc.unisonvoices = 1; }
+			}
+			if ( GSUisntEmpty( osc ) ) {
+				return [
+					Object.assign( { synths: { [ synthId ]: { oscillators: { [ oscId ]: osc } } } }, newPatObj ),
+					[ "synth", "changeOscillatorSource", syn.name, newSrcPatName ],
+				];
+			}
+		} );
 } );
