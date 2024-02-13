bae9a05bc6f4f0c58113c2b8566741a9aa89e71f
mr21 <thomastortorini@gmail.com>
2024-02-13 06:05:55 +0100

    actions/addDrumrow: use of common/addPatternBuffer

--- src/actions/addDrumrow.js
+++ src/actions/addDrumrow.js
@@ -1,17 +1,16 @@
 "use strict";
 
-DAWCoreActions.set( "addDrumrow", ( daw, pattern ) => {
-	const pat = daw.$getPattern( pattern );
+DAWCoreActions.set( "addDrumrow", ( daw, patType, patId ) => {
+	return DAWCoreActionsCommon.addPatternBuffer( daw, patType, patId )
+		.then( ( [ patId, patName, patObj ] ) => {
+			const drumrows = daw.$getDrumrows();
+			const id = DAWCoreActionsCommon.getNextIdOf( drumrows );
+			const order = DAWCoreActionsCommon.getNextOrderOf( drumrows );
+			const rowObj = DAWCoreJSON.drumrow( { order, pattern: patId } );
 
-	if ( pat.type === "buffer" ) {
-		const drumrows = daw.$getDrumrows();
-		const id = DAWCoreActionsCommon.getNextIdOf( drumrows );
-		const order = DAWCoreActionsCommon.getNextOrderOf( drumrows );
-		const rowObj = DAWCoreJSON.drumrow( { pattern, order } );
-
-		return [
-			{ drumrows: { [ id ]: rowObj } },
-			[ "drumrows", "addDrumrow", pat.name ],
-		];
-	}
+			return [
+				Object.assign( { drumrows: { [ id ]: rowObj } }, patObj ),
+				[ "drumrows", "addDrumrow", patName ],
+			];
+		} );
 } );
