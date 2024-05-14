4ab19fcc91bc87055f3a4bb89c2d4de9a71e0bc5
mr21 <thomastortorini@gmail.com>
2024-05-14 04:45:59 +0200

    DAWCoreControllers.drumrows => DAWCoreControllerDrumrows

--- src/controllers/drumrows.js
+++ src/controllers/drumrows.js
@@ -1,35 +1,33 @@
 "use strict";
 
-DAWCoreControllers.drumrows = class {
-	on = null;
-	data = Object.freeze( {
+class DAWCoreControllerDrumrows {
+	$on = null;
+	$data = Object.freeze( {
 		patterns: {},
 		drumrows: {},
 	} );
-	#drumrowsCrud = GSUcreateUpdateDelete.bind( null, this.data.drumrows,
+	#drumrowsCrud = GSUcreateUpdateDelete.bind( null, this.$data.drumrows,
 		this.#addDrumrow.bind( this ),
 		this.#updateDrumrow.bind( this ),
 		this.#deleteDrumrow.bind( this ) );
-	#patternsCrud = GSUcreateUpdateDelete.bind( null, this.data.patterns,
+	#patternsCrud = GSUcreateUpdateDelete.bind( null, this.$data.patterns,
 		this.#addPattern.bind( this ),
 		this.#updatePattern.bind( this ),
 		this.#deletePattern.bind( this ) );
 
 	constructor( fns ) {
-		this.on = GSUmapCallbacks( [
-			"addDrumrow",
-			"removeDrumrow",
-			"changeDrumrow",
-		], fns.dataCallbacks );
+		this.$on = { ...fns };
 		Object.freeze( this );
 	}
-	change( { patterns, drumrows } ) {
+
+	// .........................................................................
+	$change( { patterns, drumrows } ) {
 		if ( patterns ) { this.#patternsCrud( patterns ); }
 		if ( drumrows ) { this.#drumrowsCrud( drumrows ); }
 	}
-	clear() {
-		Object.keys( this.data.patterns ).forEach( id => delete this.data.patterns[ id ] );
-		Object.keys( this.data.drumrows ).forEach( this.#deleteDrumrow, this );
+	$clear() {
+		Object.keys( this.$data.patterns ).forEach( id => delete this.$data.patterns[ id ] );
+		Object.keys( this.$data.drumrows ).forEach( this.#deleteDrumrow, this );
 	}
 
 	// .........................................................................
@@ -37,14 +35,14 @@ DAWCoreControllers.drumrows = class {
 		if ( type === "buffer" ) {
 			const pat = Object.seal( { name, dest, buffer, duration } );
 
-			this.data.patterns[ id ] = pat;
+			this.$data.patterns[ id ] = pat;
 			this.#updatePattern( id, pat );
 		}
 	}
 	#updatePattern( id, { name, duration, dest } ) {
 		if ( name !== undefined || duration !== undefined || dest !== undefined ) {
-			const pat = this.data.patterns[ id ];
-			const rowsEnt = Object.entries( this.data.drumrows )
+			const pat = this.$data.patterns[ id ];
+			const rowsEnt = Object.entries( this.$data.drumrows )
 				.filter( kv => kv[ 1 ].pattern === id );
 
 			this.#updatePattern2( pat, rowsEnt, "name", name );
@@ -55,33 +53,33 @@ DAWCoreControllers.drumrows = class {
 	#updatePattern2( pat, rowsEnt, prop, val ) {
 		if ( val !== undefined ) {
 			pat[ prop ] = val;
-			rowsEnt.forEach( kv => this.on.changeDrumrow( kv[ 0 ], prop, val ) );
+			rowsEnt.forEach( kv => this.$on.$changeDrumrow( kv[ 0 ], prop, val ) );
 		}
 	}
 	#deletePattern( id ) {
-		delete this.data.patterns[ id ];
+		delete this.$data.patterns[ id ];
 	}
 
 	// .........................................................................
 	#addDrumrow( id, obj ) {
 		const row = Object.seal( { ...obj } );
 
-		this.data.drumrows[ id ] = row;
-		this.on.addDrumrow( id, row );
+		this.$data.drumrows[ id ] = row;
+		this.$on.$addDrumrow( id, row );
 		this.#updateDrumrow2( id, row );
 	}
 	#deleteDrumrow( id ) {
-		delete this.data.drumrows[ id ];
-		this.on.removeDrumrow( id );
+		delete this.$data.drumrows[ id ];
+		this.$on.$removeDrumrow( id );
 	}
 	#updateDrumrow( id, obj ) {
-		const row = this.data.drumrows[ id ];
+		const row = this.$data.drumrows[ id ];
 
 		Object.assign( row, obj );
 		this.#updateDrumrow2( id, obj );
 	}
 	#updateDrumrow2( id, obj ) {
-		const pat = this.data.patterns[ obj.pattern ];
+		const pat = this.$data.patterns[ obj.pattern ];
 
 		this.#updateDrumrow3( id, "order", obj.order );
 		this.#updateDrumrow3( id, "toggle", obj.toggle );
@@ -94,9 +92,9 @@ DAWCoreControllers.drumrows = class {
 	}
 	#updateDrumrow3( id, prop, val ) {
 		if ( val !== undefined ) {
-			this.on.changeDrumrow( id, prop, val );
+			this.$on.$changeDrumrow( id, prop, val );
 		}
 	}
-};
+}
 
-Object.freeze( DAWCoreControllers.drumrows );
+Object.freeze( DAWCoreControllerDrumrows );
