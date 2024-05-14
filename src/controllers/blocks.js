22ac782350c17cb8b085850158bbfc1704a870a1
mr21 <thomastortorini@gmail.com>
2024-05-14 03:46:15 +0200

    DAWCoreControllers.blocks => DAWCoreControllerBlocks + changeDuration

--- src/controllers/blocks.js
+++ src/controllers/blocks.js
@@ -1,28 +1,28 @@
 "use strict";
 
-DAWCoreControllers.blocks = class {
-	on = null;
-	data = {};
-	#blocksCrud = GSUcreateUpdateDelete.bind( null, this.data,
+class DAWCoreControllerBlocks {
+	$on = null;
+	$data = {};
+	#duration = 0;
+	#beatsPerMeasure = 4;
+	#blocksCrud = GSUcreateUpdateDelete.bind( null, this.$data,
 		this.#addBlock.bind( this ),
 		this.#updateBlock.bind( this ),
 		this.#deleteBlock.bind( this ) );
 
 	constructor( fns ) {
-		this.on = GSUmapCallbacks( [
-			"addBlock",
-			"removeBlock",
-			"changeBlockProp",
-			"updateBlockViewBox",
-		], fns.dataCallbacks );
+		this.$on = { ...fns };
 		Object.freeze( this );
 	}
 
 	// .........................................................................
-	clear() {
-		Object.keys( this.data ).forEach( this.#deleteBlock, this );
+	$setTimedivision( timediv ) {
+		this.#beatsPerMeasure = GSUsplitNums( timediv, "/" )[ 1 ];
 	}
-	change( obj ) {
+	$clear() {
+		Object.keys( this.$data ).forEach( this.#deleteBlock, this );
+	}
+	$change( obj ) {
 		this.#blocksCrud( obj.blocks );
 	}
 
@@ -30,17 +30,19 @@ DAWCoreControllers.blocks = class {
 	#addBlock( id, obj ) {
 		const blc = { ...obj };
 
-		this.data[ id ] = blc;
-		this.on.addBlock( id, blc );
+		this.$data[ id ] = blc;
+		this.#updateDuration();
+		this.$on.$addBlock( id, blc );
 		this.#updateBlock( id, blc );
 	}
 	#deleteBlock( id ) {
-		delete this.data[ id ];
-		this.on.removeBlock( id );
+		delete this.$data[ id ];
+		this.#updateDuration();
+		this.$on.$removeBlock( id );
 	}
 	#updateBlock( id, obj ) {
-		const dataBlc = this.data[ id ];
-		const cb = this.on.changeBlockProp.bind( null, id );
+		const dataBlc = this.$data[ id ];
+		const cb = this.$on.$changeBlockProp.bind( null, id );
 
 		this.#setProp( dataBlc, cb, "when", obj.when );
 		this.#setProp( dataBlc, cb, "duration", obj.duration );
@@ -48,8 +50,9 @@ DAWCoreControllers.blocks = class {
 		this.#setProp( dataBlc, cb, "track", obj.track );
 		this.#setProp( dataBlc, cb, "selected", obj.selected );
 		if ( "offset" in obj || "duration" in obj ) {
-			this.on.updateBlockViewBox( id, dataBlc );
+			this.$on.$updateBlockViewBox( id, dataBlc );
 		}
+		this.#updateDuration();
 	}
 	#setProp( data, cb, prop, val ) {
 		if ( val !== undefined ) {
@@ -57,6 +60,15 @@ DAWCoreControllers.blocks = class {
 			cb( prop, val );
 		}
 	}
-};
+	#updateDuration() {
+		const dur = Object.values( this.$data ).reduce( ( dur, blc ) => Math.max( dur, blc.when + blc.duration ), 0 );
+		const dur2 = Math.max( 1, Math.ceil( dur / this.#beatsPerMeasure ) ) * this.#beatsPerMeasure;
 
-Object.freeze( DAWCoreControllers.blocks );
+		if ( dur2 !== this.#duration ) {
+			this.#duration = dur2;
+			this.$on.$changeDuration( dur2 );
+		}
+	}
+}
+
+Object.freeze( DAWCoreControllerBlocks );
