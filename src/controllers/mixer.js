e2309d4ca72b29aef992404ddee20337843e6ca1
mr21 <thomastortorini@gmail.com>
2024-07-22 06:50:17 +0200

    controllers/mixer: data is now private

--- src/controllers/mixer.js
+++ src/controllers/mixer.js
@@ -1,33 +1,34 @@
 "use strict";
 
 class DAWCoreControllerMixer {
-	$data = Object.freeze( { channels: {}, effects: {} } );
-	#on = null;
-	#chansCrud = GSUcreateUpdateDelete.bind( null, this.$data.channels,
+	#on = {};
+	#data = Object.freeze( { channels: {}, effects: {} } );
+	#chansCrud = GSUcreateUpdateDelete.bind( null, this.#data.channels,
 		this.#addChannel.bind( this ),
 		this.#updateChannel.bind( this ),
 		this.#deleteChannel.bind( this ) );
-	#effectsCrud = GSUcreateUpdateDelete.bind( null, this.$data.effects,
+	#effectsCrud = GSUcreateUpdateDelete.bind( null, this.#data.effects,
 		this.#addEffect.bind( this ),
 		this.#updateEffect.bind( this ),
 		this.#deleteEffect.bind( this ) );
 
 	constructor( fns ) {
-		this.#on = { ...fns };
 		Object.freeze( this );
+		Object.assign( this.#on, fns );
 	}
 
 	// .........................................................................
+	$getData() { return this.#data; }
 	$clear() {
-		Object.keys( this.$data.effects ).forEach( id => this.#deleteEffect( id ) );
-		Object.keys( this.$data.channels ).forEach( id => {
+		Object.keys( this.#data.effects ).forEach( id => this.#deleteEffect( id ) );
+		Object.keys( this.#data.channels ).forEach( id => {
 			if ( id !== "main" ) {
 				this.#deleteChannel( id );
 			}
 		} );
 	}
 	$recall() {
-		const ent = Object.entries( this.$data.channels );
+		const ent = Object.entries( this.#data.channels );
 
 		ent.forEach( kv => this.#deleteChannel( kv[ 0 ] ) );
 		ent.forEach( kv => this.#addChannel( kv[ 0 ], kv[ 1 ] ) );
@@ -41,16 +42,16 @@ class DAWCoreControllerMixer {
 	#addChannel( id, obj ) {
 		const obj2 = Object.seal( { ...obj } );
 
-		this.$data.channels[ id ] = obj2;
+		this.#data.channels[ id ] = obj2;
 		this.#on.$addChannel?.( id, obj2 );
 		this.#updateChannel( id, obj2 );
 	}
 	#deleteChannel( id ) {
-		delete this.$data.channels[ id ];
+		delete this.#data.channels[ id ];
 		this.#on.$removeChannel?.( id );
 	}
 	#updateChannel( id, obj ) {
-		const chan = this.$data.channels[ id ];
+		const chan = this.#data.channels[ id ];
 		const prevCpy = { ...chan };
 
 		Object.assign( chan, obj );
@@ -62,21 +63,21 @@ class DAWCoreControllerMixer {
 
 	// .........................................................................
 	#addEffect( id, obj ) {
-		this.$data.effects[ id ] = {};
+		this.#data.effects[ id ] = {};
 		this.#on.$addEffect?.( obj.dest, id, obj );
 		this.#updateEffect( id, obj );
 	}
 	#deleteEffect( id ) {
-		const dest = this.$data.effects[ id ].dest;
+		const dest = this.#data.effects[ id ].dest;
 
-		delete this.$data.effects[ id ];
-		if ( dest in this.$data.channels ) {
+		delete this.#data.effects[ id ];
+		if ( dest in this.#data.channels ) {
 			this.#on.$removeEffect?.( dest, id );
 		}
 	}
 	#updateEffect( id, obj ) {
-		GSUdeepAssign( this.$data.effects[ id ], obj );
-		this.#on.$updateEffect?.( this.$data.effects[ id ].dest, id, obj );
+		GSUdeepAssign( this.#data.effects[ id ], obj );
+		this.#on.$updateEffect?.( this.#data.effects[ id ].dest, id, obj );
 	}
 }
 
